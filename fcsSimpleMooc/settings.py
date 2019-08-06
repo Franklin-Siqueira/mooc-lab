d9984db49025744edc207fa1c8c8cdb4f653cea5
Franklin Siqueira <franklin.siqueira@me.com>
2019-08-06 19:00:45 -0300

    Nineth commit.

--- fcsSimpleMooc/settings.py
+++ fcsSimpleMooc/settings.py
@@ -164,5 +164,5 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS =  (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 ##########                  END                ###########
