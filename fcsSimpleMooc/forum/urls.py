bc72eb4a3c108a0db511736ce2505ebaa1337561
Franklin Siqueira <franklin.siqueira@me.com>
2019-08-08 02:39:40 -0300

    commented out s3.amazonaws bucket

--- fcsSimpleMooc/forum/urls.py
+++ fcsSimpleMooc/forum/urls.py
@@ -10,7 +10,7 @@ URLs for the forum's related instances
 
 '''
 from django.contrib import admin
-from fcsSimpleMooc.forum import views
+from fcsSimpleMooc.forum.views import *
 from django.urls import path, include, re_path
 #
 #  Code from course's material
@@ -27,11 +27,11 @@ admin.autodiscover()
 
 app_name = 'forum'
 
-urlpatterns = [path('index', views.index, name = 'index'), 
-               path('tag/<tag>/', views.index, name = "index_tagged"),
-               path('replies/<pk>/correct/', views.reply_correct, name = "reply_correct"),
-               path('replies/<pk>/incorrect/', views.reply_incorrect, name = "reply_incorrect"),
-               path('<shortcut>/', views.thread, name = "thread"),]
+urlpatterns = [path('index', index, name = 'index'), 
+               path('tag/<tag>/', index, name = "index_tagged"),
+               path('replies/<pk>/correct/', reply_correct, name = "reply_correct"),
+               path('replies/<pk>/incorrect/', reply_incorrect, name = "reply_incorrect"),
+               path('<shortcut>', thread, name = "thread"),]
 
 # urlpatterns = [path('index', views.index, name = 'index'), 
 #                path('<shortcut>', views.details, name = "details"),
@@ -42,3 +42,16 @@ urlpatterns = [path('index', views.index, name = 'index'),
 #                path('<shortcut>/lessons/', views.lessons, name = "lessons"),
 #                path('<shortcut>/lessons/<pk>', views.lesson, name = "lesson"),
 #                path('<shortcut>/material/<pk>', views.material, name = "material"),]
+
+# urlpatterns = patterns('simplemooc.courses.views',
+#     url(r'^$', 'index', name='index'),
+#     url(r'^(?P<slug>[\w_-]+)/$', 'details', name='details'),
+#     url(r'^(?P<slug>[\w_-]+)/inscricao/$', 'enrollment', name='enrollment'),
+#     url(r'^(?P<slug>[\w_-]+)/cancelar-inscricao/$', 'undo_enrollment',name='undo_enrollment'),
+#     url(r'^(?P<slug>[\w_-]+)/anuncios/$', 'announcements',name='announcements'),
+#     url(r'^(?P<slug>[\w_-]+)/anuncios/(?P<pk>\d+)/$', 'show_announcement', name='show_announcement'),
+#     url(r'^(?P<slug>[\w_-]+)/aulas/$', 'lessons', name='lessons'),
+#     url(r'^(?P<slug>[\w_-]+)/aulas/(?P<pk>\d+)/$', 'lesson', name='lesson'),
+#     url(r'^(?P<slug>[\w_-]+)/materiais/(?P<pk>\d+)/$', 'material', name='material'),)
+
+
