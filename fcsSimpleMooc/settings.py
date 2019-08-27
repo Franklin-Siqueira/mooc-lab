92ef6c1ad0e6173f9b358acaa26350e07ab099df
Franklin Siqueira <franklin.siqueira@me.com>
2019-08-27 03:26:59 -0300

    Changed SITE_ID configuration

--- fcsSimpleMooc/settings.py
+++ fcsSimpleMooc/settings.py
@@ -109,7 +109,7 @@ ROOT_URLCONF = 'fcsSimpleMooc.urls'
 WSGI_APPLICATION = 'fcsSimpleMooc.wsgi.application'
 
 # for 
-SITE_ID = 2
+SITE_ID = 3
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
